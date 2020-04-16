9b81268b9761a06da91e2b3ae38a7766bd796289
Arthur <arthur@maxiru.com>
2020-04-16 22:44:20 +0200

    check

--- network.js
+++ network.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+console.log(process.env.NODE_ENV)
+
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://krasnoevino.ga',
   validateStatus: status => {
     if (status === 401) {
       localStorage.removeItem('token')
